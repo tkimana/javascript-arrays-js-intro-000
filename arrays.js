1cefe7eff859492a34299d961e2897150d38005c
Theodore Kimana <kimana.theodore@gmail.com>
2019-01-28 18:33:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,36 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
-var addElementToBeginningOfArray (["water"])
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+ function addElementToBeginningOfArray (array, name) {
+  return [name, ...array]
+ }
+ function destructivelyAddElementToBeginningOfArray (array, name) {
+   array.unshift(name)
+   return array
+ }
+function addElementToEndOfArray (array, name) {
+  return [...array, name]
+
+}
+function destructivelyAddElementToEndOfArray(array, name) {
+  array.push(name)
+  return array
+}
+function accessElementInArray (array,name) {
+  return (array[2]);
+
+}
+function destructivelyRemoveElementFromBeginningOfArray(array, name) {
+  array.shift(name)
+  return array
+}
+function removeElementFromBeginningOfArray(array, name) {
+  array= array.slice(1)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray(array, name) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray (array, name) {
+  array=array.slice(0,array.length-1)
+  return array
+}
